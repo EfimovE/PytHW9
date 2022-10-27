ea6e0e0d80bbc2f80c4268e09430859f9c5aa8e5
EfimovE <efimov_e@mail.ru>
2022-10-27 19:37:58 +0500

    save1

--- main.py
+++ main.py
@@ -16,35 +16,10 @@ print ('*******Download video*******')
 my_video = my_video.streams.get_highest_resolution()
 # or
 # my_video = my_video.streams.first()
-# print (my_video.thumbnail_url)
+
 # for stream in my_video.streams:
 #     print(streams)
 my_video.download()
 
 
 
-
-# import requests
-
-# video_url = 'https://youtu.be/gvYGIhuiJQI'
-# def download_video(url=''):
-#     try:
-#         # response = requests.get(url=url)
-#         # with open ('req_video.jpg', 'wb') as file:
-#         #     file.write(response.content)
-#         # return 'Video successfully downloaded !'
-#         response = requests.get(url=url, stream=True)
-#         with open ('req_video.mp4', 'wb') as file:
-#             for chunk in response.iter_content(chunk_size=1024*1024):
-#                 if chunk:
-#                     file.write(chunk)
-#         return 'Video successfully downloaded !'
-
-#     except Exception as _ex:
-#         return 'Upps... Check the URL, please !'
-
-# def main ():
-#     print(download_video(url=video_url))
-
-# if __name__=='__main__':
-#     main()
