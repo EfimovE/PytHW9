9960468523028433a75b5828e4000180d68f84f0
EfimovE <efimov_e@mail.ru>
2022-10-27 20:36:01 +0500

    save2

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from asyncio import streams
 from pytube import YouTube
+from pytube.exceptions import RegexMatchError
 
-url = 'https://www.youtube.com/watch?v=7BXJIjfJCsA'
-my_video = YouTube(url)
+url = input('Введите ссылку на видео Youtube:')
+# url = 'https://www.youtube.com/watch?v=7BXJIjfJCsA'
 
-print ('*******Video Title*******')
-print (my_video.title)
+def download_video(url=''):
+    try:
+        my_video = YouTube(url)
+        print ('*******Video Title*******')
+        print (my_video.title)
+        print ('*******Tumbnail Image*******')
+        print (my_video.thumbnail_url)
+        print ('*******Download video*******')
+        # set stream resolution
+        my_video = my_video.streams.get_highest_resolution()
+        # or
+        # my_video = my_video.streams.first()
+        my_video.download()
+        return 'Video successfully downloaded !'
+    # except Exception as _ex:
+    except RegexMatchError:
+        return 'Upps... Check the URL, please !'
 
-print ('*******Tumbnail Image*******')
-print (my_video.thumbnail_url)
+def main ():
+    print(download_video(url=url))
 
-print ('*******Download video*******')
+if __name__=='__main__':
+    main()
 
-# set stream resolution
-my_video = my_video.streams.get_highest_resolution()
-# or
-# my_video = my_video.streams.first()
-
-# for stream in my_video.streams:
-#     print(streams)
-my_video.download()
 
 
 
